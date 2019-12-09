ffd82199b0c132e249ba8255170e463e5638ca70
Raghvendra Sahu <rx.sahu@gmail.com>
2019-12-09 19:28:09 +0530

    assignmnet

---
+++ src/main/java/com/raghavx/demo/spring/boot/LoginController.java
@@ -0,0 +1,11 @@
+package com.raghavx.demo.spring.boot;
+
+public class LoginController {
+/**
+ * Create a user class 
+ * 
+ * Send the user as json payload 
+ * 
+ * create a mapping for the login 
+ */
+}
